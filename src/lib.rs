ecfe45bae6a42ec40054981c283db4047f7c050b
Taylor Henderson <tph5595@verizon.net>
2023-04-17 18:53:41 -0400

    landscape functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use pyo3::prelude::*;
 use rpls;
 
-#[pyfunction]
-fn pairs_to_landscape(bd_pairs: Vec<(f32, f32)>, k: usize, debug: bool) -> PyResult<Vec<Vec<(f32, f32)>>> {
-    let bd_pairs = bd_pairs.into_iter().map(|pair| rpls::birthdeath::BirthDeath{birth: pair.0, death: pair.1}).collect();
-    let landscapes = rpls::rpls::pairs_to_landscape(bd_pairs, k, debug);
+fn convert_bd_pairs(bd_pairs: Vec<(f32, f32)>) -> Vec<rpls::birthdeath::BirthDeath>{
+    bd_pairs.into_iter().map(|pair| rpls::birthdeath::BirthDeath{birth: pair.0, death: pair.1}).collect()
+}
 
-    // Convert landscape to standard types
-    Ok(landscapes
-        .unwrap()
+fn convert_landscape(landscapes: Vec<Vec<rpls::persistencelandscape::PointOrd>>) -> Vec<Vec<(f32, f32)>>{
+    landscapes
         .into_iter()
         .map(|landscape|
              landscape
@@ -16,12 +14,28 @@ fn pairs_to_landscape(bd_pairs: Vec<(f32, f32)>, k: usize, debug: bool) -> PyRes
              .map(|point| (point.x.0, point.y.0))
              .collect::<Vec<(f32, f32)>>()
              )
-        .collect::<Vec<Vec<(f32, f32)>>>())
+        .collect::<Vec<Vec<(f32, f32)>>>()
+}
+
+#[pyfunction]
+fn pairs_to_landscape(bd_pairs: Vec<(f32, f32)>, k: usize, debug: bool) -> PyResult<Vec<Vec<(f32, f32)>>> {
+    let bd_pairs = convert_bd_pairs(bd_pairs);
+    let landscapes = rpls::rpls::pairs_to_landscape(bd_pairs, k, debug).unwrap();
+
+    Ok(convert_landscape(landscapes))
+
+}
+
+#[pyfunction]
+fn pairs_to_l2_norm(bd_pairs: Vec<(f32, f32)>, k: usize, debug: bool) -> PyResult<f32> {
+    let bd_pairs = convert_bd_pairs(bd_pairs);
+    Ok(rpls::rpls::pairs_to_l2_norm(bd_pairs, k, debug))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rpls_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(pairs_to_landscape, m)?)?;
+    m.add_function(wrap_pyfunction!(pairs_to_l2_norm, m)?)?;
     Ok(())
 }
