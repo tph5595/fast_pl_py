b0dd7980f228fbda454c41764b270913ef4bf231
Taylor Henderson <tph5595@verizon.net>
2025-02-03 14:13:50 -0500

    update to 1.3.0 fast-pl

--- src/lib.rs
+++ src/lib.rs
@@ -12,31 +12,31 @@ fn convert_bd_pairs(bd_pairs: Vec<(f64, f64)>) -> Vec<fast_pl::birthdeath::Birth
     bd_pairs.into_iter().map(|pair| fast_pl::birthdeath::BirthDeath{birth: pair.0, death: pair.1}).collect()
 }
 
-fn convert_landscape(landscapes: Vec<Vec<fast_pl::persistencelandscape::PointOrd>>) -> Vec<Vec<(f64, f64)>>{
-    landscapes
-        .into_iter()
-        .map(|landscape|
-             landscape
-             .into_iter()
-             .map(|point| (point.x.0, point.y.0))
-             .collect::<Vec<(f64, f64)>>()
-             )
-        .collect::<Vec<Vec<(f64, f64)>>>()
+// fn convert_landscape(landscapes: Vec<Vec<fast_pl::persistencelandscape::PointOrd>>) -> Vec<Vec<(f64, f64)>>{
+//     landscapes
+//         .into_iter()
+//         .map(|landscape|
+//              landscape
+//              .into_iter()
+//              .map(|point| (point.x.0, point.y.0))
+//              .collect::<Vec<(f64, f64)>>()
+//              )
+//         .collect::<Vec<Vec<(f64, f64)>>>()
+// }
+
+#[pyfunction]
+fn pairs_to_landscape(bd_pairs: Vec<(f64, f64)>, k: usize, debug: bool, disable_filter: bool) -> PyResult<Vec<Vec<(f64, f64)>>> {
+    let bd_pairs = convert_bd_pairs(bd_pairs);
+    let landscapes = fast_pl::rpls::pairs_to_landscape(bd_pairs, k, debug, disable_filter).unwrap();
+
+    Ok(landscapes)
+
 }
 
 #[pyfunction]
-fn pairs_to_landscape(bd_pairs: Vec<(f64, f64)>, k: usize, debug: bool) -> PyResult<Vec<Vec<(f64, f64)>>> {
+fn pairs_to_l2_norm(bd_pairs: Vec<(f64, f64)>, k: usize, debug: bool, disable_filter: bool) -> PyResult<f64> {
     let bd_pairs = convert_bd_pairs(bd_pairs);
-    let landscapes = fast_pl::rpls::pairs_to_landscape(bd_pairs, k, debug).unwrap();
-
-    Ok(convert_landscape(landscapes))
-
-}
-
-#[pyfunction]
-fn pairs_to_l2_norm(bd_pairs: Vec<(f64, f64)>, k: usize, debug: bool) -> PyResult<f64> {
-    let bd_pairs = convert_bd_pairs(bd_pairs);
-    Ok(fast_pl::rpls::pairs_to_l2_norm(bd_pairs, k, debug).unwrap())
+    Ok(fast_pl::rpls::pairs_to_l2_norm(bd_pairs, k, debug, disable_filter).unwrap())
 }
 
 /// A Python module implemented in Rust.
